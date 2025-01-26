a45fd42a8d1e7fdf0b19284f7814706c4cd60334
BP-cmd <bhaswadip@gmail.com>
2025-01-26 17:16:01 +0530

    add basic sketch feture with design tweaks

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ for(let i = 0; i<100; i++){
   let gridRow = document.createElement('div');
   for(let j=0; j<100; j++){
    let gridCol = document.createElement('div');
+   gridCol.addEventListener("mouseover", colorGrid)
    gridRow.appendChild(gridCol);
   }
   parentGrid.appendChild(gridRow);  
 }
 
-// parentGrid.style.width = (64*10)+"px";
-// parentGrid.style.height = (64*10)+"px";
+function colorGrid(event){
+  event.target.style.backgroundColor = "black";
+};
